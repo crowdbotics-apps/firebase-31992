d9a9d97793e6095ab4c691b338263b032e30ae93
Crowdbotics <team@crowdbotics.com>
2021-11-15 08:09:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "firebase_31992",
+  url: "https://firebase_31992.botics.co",
+  api: "https://firebase_31992.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
